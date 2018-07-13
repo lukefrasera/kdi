5e85805d2b55695a780dc7a749b6685958eef2c2
Luke A Fraser <lukefrasera@gmail.com>
2018-07-12 23:20:49 -0700

    Electron is functional within the repo.

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <p>Hello World</p>
+    <kdi title="Kernel Download Installer"/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Kdi from './components/kdi.vue'
+export default {
+  name: 'app',
+  components: {
+    "kdi": Kdi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/kdi.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="kdi">
+    <h1>{{title}}</h1>
+    <h2>Available Kernels</h2>
+    <b-list-group>
+      <b-list-group-item href="#" v-for="item in items" :key="item">{{item}}</b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Kdi',
+  props: {
+    title: String
+  },
+  data() {
+    return {
+      items: [
+        "Kernel: v4.17.4",
+        "Kernel: v4.17.4",
+        "Kernel: v4.17.4"
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
