1688c76fccba8774cb4cb2964a3007d6ff414015
Luke A Fraser <lukefrasera@gmail.com>
2018-07-20 22:50:39 -0700

    Adding main package sources

--- src/elect_app.js
+++ src/elect_app.js
@@ -1,6 +1,7 @@
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
+var zerorpc = require('zerorpc');
 
 let url
 if (process.env.NODE_ENV === 'DEV') {
@@ -8,6 +9,13 @@ if (process.env.NODE_ENV === 'DEV') {
 } else {
   url = `file://${process.cwd()}/dist/index.html`
 }
+
+let client = new zerorpc.Client();
+client.connect("tcp://127.0.0.1:4242");
+
+client.invoke("sum", 10, 30, (err, ret, more) => {
+  console.log(ret);
+});
 
 app.on('ready', () => {
   let window = new BrowserWindow({width: 800, height: 600})
